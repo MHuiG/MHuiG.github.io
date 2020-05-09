56b966a4eb6ef2c8dd000398d98e5a6325da930a
MHuiG <MHuiG@users.noreply.github.com>
2020-05-09 14:46:31 +0800

    deploy: MHuiG/blog-source@71567c019faaa140a20aaed801d70ad753fae98d

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/css/style.css","ddb81a2094d2d0c03de3813b7bb3f70a"],["/fonts/Monaco.ttf","7d1b5cf51af724a2641a89a881b342fe"],["/fonts/Skranji-Regular.ttf","0f860580e235e4ae4ae655c2bbb9c943"],["/fonts/Ubuntu-Regular.ttf","75adbf87abbf62e27f6a738caeb71f75"],["/js/app.js","47576ef8179a65d722d75cae729b05a1"],["/js/search.js","8c5e55f8a12105822ef6793c880f0aa7"]];
+var precacheConfig = [["/css/style.css","ddb81a2094d2d0c03de3813b7bb3f70a"],["/favicon.ico","d0cd815a171f58c431cb037873f4ef9a"],["/fonts/Monaco.ttf","7d1b5cf51af724a2641a89a881b342fe"],["/fonts/Skranji-Regular.ttf","0f860580e235e4ae4ae655c2bbb9c943"],["/fonts/Ubuntu-Regular.ttf","75adbf87abbf62e27f6a738caeb71f75"],["/js/app.js","47576ef8179a65d722d75cae729b05a1"],["/js/search.js","8c5e55f8a12105822ef6793c880f0aa7"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
@@ -288,6 +288,9 @@ self.addEventListener('fetch', function(event) {
 toolbox.router.get("/*", toolbox.cacheFirst, {"origin":"cdn.jsdelivr.net"});
 toolbox.router.get("/*", toolbox.cacheFirst, {"origin":"cdnjs.cloudflare.com"});
 toolbox.router.get("/*", toolbox.cacheFirst, {"origin":"m7.music.126.net"});
+toolbox.router.get("/*", toolbox.cacheFirst, {"origin":"ajax.cloudflare.com"});
+toolbox.router.get("/*", toolbox.cacheFirst, {"origin":"blog.mhuig.top"});
+toolbox.router.get("/*", toolbox.cacheFirst, {"origin":"mhuig.github.io"});
 
 
 
