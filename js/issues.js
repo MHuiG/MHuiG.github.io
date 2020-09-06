a82b13908fb8703d69f38c0c3227e0e182a099db
MHuiG <MHuiG@users.noreply.github.com>
2020-09-06 17:09:01 +0800

    up MHuiG/blog-source@484fbcd9f47e0c84176a022690b4473a84f8d267

--- js/issues.js
+++ js/issues.js
@@ -1,1 +1,192 @@
-function parseIssuesData(e,n){var s=new Object;if(0<n.length)for(i=0;i<n.length;i++){var t=n[i].body.match(/group:[^\n]*\n/);if(t&&0<t.length&&(t=t[0].replace(/(group:[\s]*|[\r\n]*)/g,"").split(", ")),t)for(var a=0;a<t.length;a++){var r,l=t[a];l in s?s[l].push(n[i]):((r=new Array).push(n[i]),s[l]=r)}else{var o=s[""];null==o&&(o=new Array),o.push(n[i]),s[""]=o}}return s}function getIssuesAPIForSites(h){var f=$(h.el)[0];$(f).append('<div class="loading"><i class="fa fa-cog fa-2x fa-spin"></i><p>正在加载</p></div>');var r=10;!function a(){return new Promise(function(n,e){var s=0,t=setTimeout(function(){0===s&&(s=2,t=null,e("超时"),0==r&&($(f).find(".loading i").remove(),$(f).find(".loading p").text("加载失败，请稍后重试。")))},5e3);fetch(h.api).then(function(e){if(2!==s&&(clearTimeout(t),n(e),t=null,s=1),e.ok)return e.json();throw new Error("Network response was not ok.")}).then(function(e){$(f).find(".loading").remove();var n=parseIssuesData(h,e);for(null==h.group&&(h.group=Object.keys(n)),i=0;i<h.group.length;i++){var s=h.group[i];if(!(0<s.length&&null==n[s])){var t=n[s];for(0<s.length?$(f).append("<h2>"+s+"</h2>"):""==s&&1<h.group.length&&$(f).append("<h2>未分组</h2>"),$(f).append('<div class="site-card-group '+i+'"></div>'),j=0;j<t.length;j++){var a=t[j],r=a.body.match(/title:[^\n]*\n/);r&&0<r.length&&(r=r[0].replace(/(title:[\s]*|[\r\n]*)/g,""));var l=a.body.match(/screenshot:[^\n]*\n/);l&&0<l.length&&(l=l[0].replace(/(screenshot:[\s]*|[\r\n]*)/g,""));var o=a.body.match(/avatar:[^\n]*\n/);o&&0<o.length&&(o=o[0].replace(/(avatar:[\s]*|[\r\n]*)/g,""));var c=a.body.match(/description:[^\n]*\n/);c&&0<c.length&&(c=c[0].replace(/(description:[\s]*|[\r\n]*)/g,""));var p=a.body.match(/url:[^\n]*\n/);p&&0<p.length&&(p=p[0].replace(/(url:[\s]*|[\r\n]*)/g,""));var d=a.body.match(/keywords:[^\n]*\n/);d&&0<d.length&&(d=(d=d[0].replace(/(keywords:[\s]*|[\r\n]*)/g,"")).replace("[","").replace("]","").split(", "));var g=l&&0<l.length?'<div class="img"><img src="'+l+'" onerror="javascript:this.src=\'https://cdn.jsdelivr.net/gh/volantis-x/cdn-wallpaper-minimalist/2020/052.jpg\';"/></div>':'<div class="img"></div>',u='<div class="info">';o&&0<o.length&&(u+='<img src="'+o+'" onerror="javascript:this.src=\'https://cdn.jsdelivr.net/gh/volantis-x/cdn-wallpaper-minimalist/2020/052.jpg\';"/>');var v="<a class='site-card' target='_blank' href='"+p+"'>"+g+(u+='<span class="title">'+r+'</span><span class="desc">'+(c||d&&d.join("、"))+"</span></div>")+"</a>";$(f).find(".site-card-group."+i).append(v)}}}}).catch(function(e){0<r?(--r,$(f).find(".loading p").text("加载失败，正在重试..."),setTimeout(function(){a()},5e3)):($(f).find(".loading i").remove(),$(f).find(".loading p").text("加载失败，请稍后重试。"))})})}()}function getIssuesAPIForTimeline(e){var a=$(e.el)[0];$(a).append('<div class="loading"><i class="fa fa-cog fa-2x fa-spin"></i><p>正在加载</p></div>'),$.get(e.api,function(e,n){if($(a).find(".loading").remove(),0<e.length)for(i=0;i<e.length;i++){var s='&nbsp;&nbsp;<a class="comments" target="_blank" href="'+e[i].html_url+'"><i class="fa fa-comment-dots fa-fw"></i>'+e[i].comments+"</a>",t='<div class="timenode"><div class="meta"><p></p><p>'+e[i].title+s+'</p><p></p></div><div class="body"><p>'+e[i].body+"</p></div></div>";$(a).append(t)}})}function checkIssues(){for(var e=document.getElementsByClassName("issues-api"),n=0;n<e.length;n++){var i=e[n],s=i.getAttribute("api"),t=i.getAttribute("group"),a=new Object;a.class=i.getAttribute("class"),a.el=i,a.api=s,a.group=t?t.replace(/(,[\s]*)/g,",").split(","):void 0,a.class.split(" ").includes("sites")?getIssuesAPIForSites(a):a.class.split(" ").includes("timeline")&&getIssuesAPIForTimeline(a)}}document.addEventListener("DOMContentLoaded",function(){checkIssues()}),document.addEventListener("pjax:complete",function(){checkIssues()});
+function parseIssuesData(cfg, data) {
+  let groups = new Object();
+  if (data.length > 0) {
+    for (i = 0; i < data.length; i++) {
+      // get categories
+      let group = data[i].body.match(/group:[^\n]*\n/);
+      if (group && group.length > 0) {
+        group = group[0].replace(/(group:[\s]*|[\r\n]*)/g,'').split(', ');
+      }
+      if (group) {
+        for (var j = 0; j < group.length; j++) {
+          let tmp = group[j];
+          if (tmp in groups) {
+            groups[tmp].push(data[i]);
+          } else {
+            let arr = new Array();
+            arr.push(data[i]);
+            groups[tmp] = arr;
+          }
+        }
+      } else {
+        let arr = groups[''];
+        if (arr == undefined) {
+          arr = new Array();
+        }
+        arr.push(data[i]);
+        groups[''] = arr;
+      }
+    }
+  }
+  return groups;
+}
+
+function getIssuesAPIForSites(cfg) {
+  let el = $(cfg.el)[0];
+  $(el).append('<div class="loading"><i class="fa fa-cog fa-2x fa-spin"></i><p>正在加载</p></div>');
+
+  let retryTimes = 10;
+  function request() {
+    return new Promise((resolve, reject) => {
+      let status = 0; // 0 等待 1 完成 2 超时
+      let timer = setTimeout(() => {
+        if (status === 0) {
+          status = 2;
+          timer = null;
+          reject("超时");
+          if (retryTimes == 0) {
+            $(el).find('.loading i').remove();
+            $(el).find('.loading p').text('加载失败，请稍后重试。');
+          }
+        }
+      }, 5000);
+      fetch(cfg.api).then(function(response) {
+        if (status !== 2) {
+          clearTimeout(timer);
+          resolve(response);
+          timer = null;
+          status = 1;
+        }
+        if(response.ok) {
+          return response.json();
+        }
+        throw new Error('Network response was not ok.');
+      }).then(function(data) {
+        $(el).find('.loading').remove();
+        let dt = parseIssuesData(cfg, data);
+        if (cfg.group == undefined) {
+          cfg.group = Object.keys(dt);
+        }
+        for (i = 0; i < cfg.group.length; i++) {
+          let name = cfg.group[i];
+          if (name.length > 0) {
+            if (dt[name] == undefined) {
+              continue;
+            }
+          }
+          let issues = dt[name];
+          if (name.length > 0) {
+            $(el).append('<h2>' + name + '</h2>');
+          } else if (name == '' && cfg.group.length > 1) {
+            $(el).append('<h2>' + '未分组' + '</h2>');
+          }
+          $(el).append('<div class="site-card-group ' + i + '"></div>');
+          // layout items
+          for (j = 0; j < issues.length; j++) {
+            let issue = issues[j];
+            // get title
+            let title = issue.body.match(/title:[^\n]*\n/);
+            if (title && title.length > 0) {
+              title = title[0].replace(/(title:[\s]*|[\r\n]*)/g,'');
+            }
+            // get screenshot
+            let screenshot = issue.body.match(/screenshot:[^\n]*\n/);
+            if (screenshot && screenshot.length > 0) {
+              screenshot = screenshot[0].replace(/(screenshot:[\s]*|[\r\n]*)/g,'');
+            }
+            // get avatar
+            let avatar = issue.body.match(/avatar:[^\n]*\n/);
+            if (avatar && avatar.length > 0) {
+              avatar = avatar[0].replace(/(avatar:[\s]*|[\r\n]*)/g,'');
+            }
+            // get desc
+            let desc = issue.body.match(/description:[^\n]*\n/);
+            if (desc && desc.length > 0) {
+              desc = desc[0].replace(/(description:[\s]*|[\r\n]*)/g,'');
+            }
+            // get url
+            let url = issue.body.match(/url:[^\n]*\n/);
+            if (url && url.length > 0) {
+              url = url[0].replace(/(url:[\s]*|[\r\n]*)/g,'');
+            }
+            // get keywords
+            let keywords = issue.body.match(/keywords:[^\n]*\n/);
+            if (keywords && keywords.length > 0) {
+              keywords = keywords[0].replace(/(keywords:[\s]*|[\r\n]*)/g,'');
+              keywords = keywords.replace('[','').replace(']','').split(', ');
+            }
+
+            let imgTag = '';
+            if (screenshot && screenshot.length > 0) {
+              imgTag = '<div class="img"><img src="' + screenshot + '" onerror="javascript:this.src=\'https://cdn.jsdelivr.net/gh/volantis-x/cdn-wallpaper-minimalist/2020/052.jpg\';"/></div>';
+            } else {
+              imgTag = '<div class="img"></div>';
+            }
+            let infoTag = '<div class="info">';
+            if (avatar && avatar.length > 0) {
+              infoTag += '<img src="' + avatar + '" onerror="javascript:this.src=\'https://cdn.jsdelivr.net/gh/volantis-x/cdn-wallpaper-minimalist/2020/052.jpg\';"/>';
+            }
+            infoTag += '<span class="title">' + title + '</span><span class="desc">' + (desc || keywords && keywords.join('、')) + '</span></div>';
+            let cardTag = "<a class='site-card' target='_blank' href='" + url + "'>" + imgTag + infoTag + "</a>";
+            $(el).find('.site-card-group.' + i).append(cardTag);
+          }
+        }
+      }).catch(function(error) {
+        if (retryTimes > 0) {
+          retryTimes -= 1;
+          $(el).find('.loading p').text('加载失败，正在重试...');
+          setTimeout(() => {
+            request();
+          }, 5000);
+        } else {
+          $(el).find('.loading i').remove();
+          $(el).find('.loading p').text('加载失败，请稍后重试。');
+        }
+      });
+    });
+  }
+  request();
+}
+
+function getIssuesAPIForTimeline(cfg) {
+  let el = $(cfg.el)[0];
+  $(el).append('<div class="loading"><i class="fa fa-cog fa-2x fa-spin"></i><p>正在加载</p></div>');
+  $.get(cfg.api, function(data, status) {
+    $(el).find('.loading').remove();
+    if (data.length > 0) {
+      for (i = 0; i < data.length; i++) {
+        let a = '&nbsp;&nbsp;<a class="comments" target="_blank" href="' + data[i].html_url + '"><i class="fa fa-comment-dots fa-fw"></i>' + data[i].comments + '</a>';
+        let meta = '<div class="meta"><p></p><p>' + data[i].title + a + '</p><p></p></div>';
+        let body = '<div class="body"><p>' + data[i].body + '</p></div>';
+        let tag = '<div class="timenode">' + meta + body + '</div>';
+        $(el).append(tag);
+      }
+    }
+  });
+}
+
+function checkIssues() {
+  let els = document.getElementsByClassName('issues-api');
+  for (var i = 0; i < els.length; i++) {
+    let el = els[i];
+    let api = el.getAttribute('api');
+    let group = el.getAttribute('group');
+    var cfg = new Object();
+    cfg.class = el.getAttribute('class');
+    cfg.el = el;
+    cfg.api = api;
+    cfg.group = group ? group.replace(/(,[\s]*)/g,',').split(',') : undefined;
+    if (cfg.class.split(' ').includes('sites')) {
+      getIssuesAPIForSites(cfg);
+    } else if (cfg.class.split(' ').includes('timeline')) {
+      getIssuesAPIForTimeline(cfg);
+    }
+
+  }
+}
+document.addEventListener('DOMContentLoaded', function () {
+  checkIssues();
+});
+document.addEventListener('pjax:complete', function () {
+  checkIssues();
+});
--- s.js
+++ s.js
@@ -1,1 +1,165 @@
-importScripts("https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.3/workbox/workbox-sw.js"),workbox.setConfig({modulePathPrefix:"https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.3/workbox/"});var _workbox=workbox,core=_workbox.core,precaching=_workbox.precaching,routing=_workbox.routing,strategies=_workbox.strategies,expiration=_workbox.expiration,cacheableResponse=_workbox.cacheableResponse,backgroundSync=_workbox.backgroundSync,CacheFirst=strategies.CacheFirst,NetworkFirst=strategies.NetworkFirst,NetworkOnly=strategies.NetworkOnly,StaleWhileRevalidate=strategies.StaleWhileRevalidate,ExpirationPlugin=expiration.ExpirationPlugin,CacheableResponsePlugin=cacheableResponse.CacheableResponsePlugin,cacheSuffixVersion="-000011",maxEntries=100;self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(e){return Promise.all(e.map(function(e){if(!e.includes(cacheSuffixVersion))return caches.delete(e)}))}))}),core.setCacheNameDetails({prefix:"mhuig-blog",suffix:cacheSuffixVersion}),core.skipWaiting(),core.clientsClaim(),precaching.cleanupOutdatedCaches(),precaching.precacheAndRoute([{url:"/css/style.css",revision:null},{url:"/js/app.js",revision:null},{url:"https://cdn.jsdelivr.net/npm/mhg@0.0.6/js/search.js",revision:null}]),routing.registerRoute(/.*cdn\.jsdelivr\.net/,new CacheFirst({cacheName:"static-immutable"+cacheSuffixVersion,fetchOptions:{mode:"cors",credentials:"omit"},plugins:[new ExpirationPlugin({maxAgeSeconds:2592e3,purgeOnQuotaError:!0})]})),routing.registerRoute(/.*cdnjs\.cloudflare\.com/,new CacheFirst({cacheName:"static-immutable"+cacheSuffixVersion,fetchOptions:{mode:"cors",credentials:"omit"},plugins:[new ExpirationPlugin({maxAgeSeconds:2592e3,purgeOnQuotaError:!0})]})),routing.registerRoute(/.*m7\.music\.126\.net/,new CacheFirst({cacheName:"static-immutable"+cacheSuffixVersion,fetchOptions:{mode:"cors",credentials:"omit"},plugins:[new ExpirationPlugin({maxAgeSeconds:2592e3,purgeOnQuotaError:!0})]})),routing.registerRoute(/.*baidu\.com.*/,new NetworkOnly),routing.registerRoute(/https:\/\/rmt\.dogedoge\.com.*/,new NetworkOnly),routing.registerRoute(/https:\/\/ip\..*/,new NetworkOnly),routing.registerRoute(/.*\.(?:png|jpg|jpeg|svg|gif|webp|ico|eot|ttf|woff|woff2|mp3)/,new StaleWhileRevalidate),routing.registerRoute(/.*\.(css|js)/,new StaleWhileRevalidate),routing.registerRoute("/s.js",new StaleWhileRevalidate),routing.setDefaultHandler(new NetworkFirst({networkTimeoutSeconds:3}));
+importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.3/workbox/workbox-sw.js');
+
+workbox.setConfig({
+    modulePathPrefix: 'https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.3/workbox/'
+});
+
+const { core, precaching, routing, strategies, expiration, cacheableResponse, backgroundSync } = workbox;
+const { CacheFirst, NetworkFirst, NetworkOnly, StaleWhileRevalidate } = strategies;
+const { ExpirationPlugin } = expiration;
+const { CacheableResponsePlugin } = cacheableResponse;
+
+const cacheSuffixVersion = '-000011',
+    // precacheCacheName = core.cacheNames.precache,
+    // runtimeCacheName = core.cacheNames.runtime,
+    maxEntries = 100;
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(keys.map((key) => {
+                if (!key.includes(cacheSuffixVersion)) return caches.delete(key);
+            }));
+        })
+    );
+});
+
+
+core.setCacheNameDetails({
+    prefix: 'mhuig-blog',
+    suffix: cacheSuffixVersion
+});
+
+core.skipWaiting();
+core.clientsClaim();
+precaching.cleanupOutdatedCaches();
+
+/*
+ * Precache
+ * - Static Assets
+ */
+precaching.precacheAndRoute(
+    [
+        { url: '/css/style.css', revision: null },
+        { url: '/js/app.js', revision: null },
+        { url: 'https://cdn.jsdelivr.net/npm/mhg@0.0.6/js/search.js', revision: null },
+    ],
+);
+
+/*
+ * Cache File From CDN
+ *
+ * Method: CacheFirst
+ * cacheName: static-immutable
+ * cacheTime: 30d
+ */
+
+// cdn.jsdelivr.net - cors enabled
+routing.registerRoute(
+    /.*cdn\.jsdelivr\.net/,
+    new CacheFirst({
+        cacheName: 'static-immutable' + cacheSuffixVersion,
+        fetchOptions: {
+            mode: 'cors',
+            credentials: 'omit'
+        },
+        plugins: [
+            new ExpirationPlugin({
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+                purgeOnQuotaError: true
+            })
+        ]
+    })
+);
+
+// cdnjs.cloudflare.com - cors enabled
+routing.registerRoute(
+    /.*cdnjs\.cloudflare\.com/,
+    new CacheFirst({
+        cacheName: 'static-immutable' + cacheSuffixVersion,
+        fetchOptions: {
+            mode: 'cors',
+            credentials: 'omit'
+        },
+        plugins: [
+            new ExpirationPlugin({
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+                purgeOnQuotaError: true
+            })
+        ]
+    })
+);
+// m7.music.126.net - cors enabled
+routing.registerRoute(
+    /.*m7\.music\.126\.net/,
+    new CacheFirst({
+        cacheName: 'static-immutable' + cacheSuffixVersion,
+        fetchOptions: {
+            mode: 'cors',
+            credentials: 'omit'
+        },
+        plugins: [
+            new ExpirationPlugin({
+                maxAgeSeconds: 30 * 24 * 60 * 60,
+                purgeOnQuotaError: true
+            })
+        ]
+    })
+);
+
+/*
+ *  No Cache
+ *
+ * Method: networkOnly
+ */
+routing.registerRoute(
+    /.*baidu\.com.*/,
+    new NetworkOnly()
+);
+routing.registerRoute(
+    /https:\/\/rmt\.dogedoge\.com.*/,
+    new NetworkOnly()
+);
+routing.registerRoute(
+    /https:\/\/ip\..*/,
+    new NetworkOnly()
+);
+/*
+ * Others img fonts
+ * Method: staleWhileRevalidate
+ */
+routing.registerRoute(
+    // Cache image fonts files
+    /.*\.(?:png|jpg|jpeg|svg|gif|webp|ico|eot|ttf|woff|woff2|mp3)/,
+    new StaleWhileRevalidate()
+);
+
+/*
+ * Static Assets
+ * Method: staleWhileRevalidate
+ */
+routing.registerRoute(
+    // Cache CSS files
+    /.*\.(css|js)/,
+    // Use cache but update in the background ASAP
+    new StaleWhileRevalidate()
+);
+
+/*
+ * sw.js - Revalidate every time
+ * staleWhileRevalidate
+ */
+routing.registerRoute(
+    '/s.js',
+    new StaleWhileRevalidate()
+);
+
+/*
+ * Default - Serve as it is
+ * networkFirst
+ */
+routing.setDefaultHandler(
+    new NetworkFirst({
+        networkTimeoutSeconds: 3
+    })
+);
